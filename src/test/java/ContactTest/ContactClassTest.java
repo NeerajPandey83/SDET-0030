4cc49ecca764b103ce4a83c4e7c75f3070fd29a0
91834 <91834@NARAYANA>
2022-03-16 10:34:00 +0530

    NEERAJ committe

---
+++ src/test/java/ContactTest/ContactClassTest.java
@@ -0,0 +1,12 @@
+package ContactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactClassTest {
+
+	@Test
+	public void contact()
+	{
+		System.out.println("Done");
+	}
+}
---
+++ src/test/java/OrgnizationTest/BookingFlight.java
@@ -0,0 +1,25 @@
+package OrgnizationTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BookingFlight {
+	
+
+	@Test
+	public void bookIndigoFlight()
+	{
+		//ChromeOptions option = new ChromeOptions();
+		//option.addArguments("--disable-notifications");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.goindigo.in/");
+		driver.manage().deleteAllCookies();
+		driver.findElement(By.xpath("//label[.='Round Trip']")).click();
+		driver.findElement(By.xpath("(//div[@class='airport-city'])[1]"));
+		
+	}
+
+}
